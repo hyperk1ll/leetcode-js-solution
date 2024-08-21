f558109673aa86f2a366deb9bfebd040fe8802b1
Hilmy Tsany <hilmy.tsani@gmail.com>
2024-08-21 17:46:37 +0800

    4. Median of Two Sorted Arrays

---
+++ 4.js
@@ -0,0 +1,19 @@
+// 4. Median of Two Sorted Arrays
+
+/**
+ * @param {number[]} nums1
+ * @param {number[]} nums2
+ * @return {number}
+ */
+var findMedianSortedArrays = function(nums1, nums2) {
+    let arr = nums1.concat(nums2).sort(function (a, b) {  return a - b;  })
+
+    const middleIndex = Math.floor(arr.length / 2);
+
+    if (arr.length % 2 === 0) {
+        return (arr[middleIndex - 1] + arr[middleIndex]) / 2;
+    } else {
+        return arr[middleIndex];
+    }
+    
+};
